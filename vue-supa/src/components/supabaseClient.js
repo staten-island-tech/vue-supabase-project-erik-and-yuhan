4a85327d6d62a79670f73bb19c3866d771b75394
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-04-03 09:05:35 -0400

    ghfgh

---
+++ vue-supa/src/components/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://okuodkgtwennhrxilnjh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdW9ka2d0d2VubmhyeGlsbmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNDkyMzMsImV4cCI6MjAyNzcyNTIzM30.-ipk6tUTb6myES2Pphqd5b5_fX3iuCkT4fPgHrRjnN0')

88e28101c16843ef001e90c672306e7bc91be0a8
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-04-04 09:05:36 -0400

    fsdf

--- vue-supa/src/App.vue
+++ vue-supa/src/App.vue
@@ -4,19 +4,33 @@ import { supabase } from './components/supabaseClient'
 
 const countries = ref([])
 
+
 async function getCountries() {
   const { data } = await supabase.from('countries').select()
   countries.value = data
   console.log(data)
 }
+const persons = ref([])
+async function getNames() {
+  const { data } = await supabase.from('persons').select()
+  persons.value = data
+  console.log(data) 
+}
 
 onMounted(() => {
   getCountries()
+  getNames()
 })
 </script>
 
 <template>
+
+<ul>
+    <li v-for="person in persons" :key="person.id">{{ person.Name }}</li>
+  </ul>
   <ul>
     <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
   </ul>
+
+ 
 </template>

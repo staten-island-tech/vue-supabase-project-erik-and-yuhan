f3873e5628300874e1468304a35d70d8f98ff5cc
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-05-16 09:10:26 -0400

    dsfs

--- vue-supa/src/App.vue
+++ vue-supa/src/App.vue
@@ -19,13 +19,6 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-<<<<<<< HEAD
-import { supabase } from './components/supabaseClient'
-import Auth from './components/AuthSupa.vue'
-import Account from './components/AccountSupa.vue'
-import SignIn from './components/SignIn.vue'
-=======
->>>>>>> yuhan
 
 //connects input
 let email = ref("")
@@ -39,51 +32,13 @@ function CreateAccount() {
 function login() {
   console.log ("login")
 }
-<<<<<<< HEAD
-
-
-const session = ref()
-
-onMounted(() => {
-
-  supabase.auth.getSession().then(({ data }) => {
-    session.value=data.session
-  })
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-
-  getCountries()
-  getNames()
-})
-
-
-
-
-
-</script>
-
-<template>
-
-  <div class="container" style="padding: 50px 0 100px 0">
-    <Account v-if="session" :session="session" />
-    <Auth v-else />
-    <SignIn></SignIn>
-    
-  </div>
-
-
-</template>
-=======
 //seeCurrentuser
 function seeCurrentuser() {
   console.log ("seeCurrentUser")
 }
 //logout
 function logout () {
-  console.log ("logot")
+  console.log ("logout")
 }
 </script>
 
->>>>>>> yuhan
